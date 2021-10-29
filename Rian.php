8ebeebb8244e12255dbb620bc016d1be40dae10a
Rian Syahrillah aspari <rianaspari22@gmai.com>
2021-10-29 18:40:42 +0700

    Lthn

---
+++ Rian.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Latihan Pemrograman Internet</title>
+    </head>
+        <body>
+            <?php
+            $nama = "mahasiswa";
+
+                echo "Nama   : Rian Syahrillah Aspari";
